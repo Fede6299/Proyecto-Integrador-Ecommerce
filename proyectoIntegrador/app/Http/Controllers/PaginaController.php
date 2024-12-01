ceb61f142d381583dc6ac721982b38fb8644bb6a
MarcosBenegas47 <marcobenega@gmail.com>
2024-12-01 18:13:31 -0300

    imges ok

--- proyectoIntegrador/app/Http/Controllers/PaginaController.php
+++ proyectoIntegrador/app/Http/Controllers/PaginaController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\Storage;
+
 
 class PaginaController extends Controller{
     public function indexa(){
+
         return view("principal");
     }
 }
--- proyectoIntegrador/resources/views/principal.blade.php
+++ proyectoIntegrador/resources/views/principal.blade.php
@@ -1,3 +1,36 @@
 @include('/layout/header')
-<h1>Vista principal</h1>
+
+<main class="container">
+  width
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner h-100">
+    <div class="carousel-item active">
+      <img src="{{ asset('./img/1.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('./img/2.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="{{ asset('./img/3.jpg') }}" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</main>
+
+
+
 @include('/layout/footer')
+
