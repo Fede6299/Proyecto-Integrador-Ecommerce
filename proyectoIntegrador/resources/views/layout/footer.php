31d4f387e86b264d076f9aeef479f67916cfe8af
Fede6299 <fapaza@frba.utn.edu.ar>
2024-12-02 12:37:17 -0300

    footer base

--- proyectoIntegrador/resources/views/layout/footer.php
+++ proyectoIntegrador/resources/views/layout/footer.php
@@ -1,8 +1,14 @@
-
-
-        <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-        
-        </footer>
+            <div class="container">
+                <footer class="py-3 my-4">
+                    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Inicio</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Productos</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Sobre Nosotros</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contacto</a></li>
+                    </ul>
+                    <p class="text-center text-body-secondary">© 2024 Mattioli</p>
+                </footer>
+            </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     </body>
