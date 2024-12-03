d2ec4e5b982a07ad1289929180b1f49d36e120d6
Gabriela Choque <nadehsko@hotmail.com>
2024-12-03 15:48:18 -0300

    Se agrego la vista de Contactos y sus estilos .

--- proyectoIntegrador/resources/views/contacto.blade.php
+++ proyectoIntegrador/resources/views/contacto.blade.php
@@ -1,3 +1,29 @@
 @include('/layout/header')
-<h2>Contacto</h2>
+<div class="cuerpoContato">
+    <h2 class="tituloContacto">Formulario de contacto</h2>
+    <div class="contact-form-container">
+
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" id="nombre" name="nombre" placeholder="Nombre" required>
+            </div>
+            <div class="form-group">
+                <label for="apellido">Apellido</label>
+                <input type="text" id="apellido" name="apellido" placeholder="Apellido" required>
+            </div>
+            <div class="form-group">
+                <label for="telefono">Número de teléfono</label>
+                <input type="tel" id="telefono" name="telefono" placeholder="Número de teléfono" required>
+            </div>
+            <div class="form-group">
+                <label for="comentarios">Comentarios</label>
+                <textarea id="comentarios" name="comentarios" rows="4" placeholder="Comentarios" required></textarea>
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
+</div>
+
+
 @include('/layout/footer')
