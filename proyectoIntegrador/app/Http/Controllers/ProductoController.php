60df6eaddc8995a88dc6d8f4422f0adab509f924
MarcosBenegas47 <marcobenega@gmail.com>
2024-12-10 19:43:00 -0300

    objeto producto

--- proyectoIntegrador/app/Http/Controllers/ProductoController.php
+++ proyectoIntegrador/app/Http/Controllers/ProductoController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\NombreLink;
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class ProductoController extends Controller
 {
     public function index($queryLink){
-        return view("producto",["queryLink" => $queryLink]);
+        $producto = NombreLink::where('nombreLink','LiKE', $queryLink)->with("producto")->first();
+        return view("producto",["producto" => $producto]);
     }
 }
--- proyectoIntegrador/resources/views/producto.blade.php
+++ proyectoIntegrador/resources/views/producto.blade.php
@@ -1,6 +1,7 @@
 @include('/layout/header')
 <main>
-    <h1>{{$queryLink}}</h1>
+    
+    <h1>{{$producto->producto->descripcion}}</h1>
 </main>
 
 @include('/layout/footer')
