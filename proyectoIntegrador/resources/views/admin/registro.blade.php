5d61fca2d3bbf4f1403a19010c4240ac3830092d
Gabriela Choque <nadehsko@hotmail.com>
2024-12-20 19:56:04 -0300

    Mini arreglo  de una class que afectaba otros estilos .

--- proyectoIntegrador/resources/views/admin/registro.blade.php
+++ proyectoIntegrador/resources/views/admin/registro.blade.php
@@ -11,37 +11,37 @@
         <div>
             <label for="nombre">Nombre:</label>
             <input name="nombre" placeholder="Nombre" type="text">
-        @error("nombre")
-            <p>{{$message}}</p>
-        @enderror
+                @error("nombre")
+                <p>{{$message}}</p>
+                @enderror
         </div>
         <div>
             <label for="apellido">Apellido:</label>
             <input name="apellido" placeholder="Apellido" type="text">
-        @error("apellido")
+             @error("apellido")
             <p>{{$message}}</p>
-        @enderror
+             @enderror
         </div>
         <div>
             <label for="user_name">Nombre de usuario:</label>
             <input name="user_name" placeholder="Nombre de usuario" type="text">
-        @error("userName")
+             @error("userName")
             <p>{{$message}}</p>
-        @enderror
+             @enderror
         </div>
         <div>
             <label for="email">Correo:</label>
             <input name="email" placeholder="Correo" type="email">
-        @error("email")
+            @error("email")
             <p>{{$message}}</p>
-        @enderror
+            @enderror
         </div>
         <div>
             <label for="password">Contraseña:</label>
             <input name="password" placeholder="Contraseña" type="password">
-        @error("password")
+            @error("password")
             <p>{{$message}}</p>
-        @enderror
+             @enderror
         </div>
         <div>
             <label for="password_confirmation">Repita la contraseña:</label>
