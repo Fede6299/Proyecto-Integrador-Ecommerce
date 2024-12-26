680063e2a82693f9e500d38adee4f8befc081efe
Nico <31677657+Nico-0@users.noreply.github.com>
2024-12-26 14:31:51 -0300

    base actual

--- db/ecommerce_mate.sql
+++ db/ecommerce_mate.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-12-2024 a las 05:14:38
+-- Tiempo de generación: 26-12-2024 a las 17:29:12
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -44,7 +44,7 @@ CREATE TABLE `admin_user` (
 --
 
 INSERT INTO `admin_user` (`id_admin`, `nombre`, `user_name`, `apellido`, `password`, `email`, `activo`, `updated_at`, `created_at`) VALUES
-(1, 'marcos', 'mbenegas', 'apellido', '12345', 'correo@correo.com', NULL, '2024-12-18 04:09:19', '2024-12-18 04:09:19');
+(2, 'marcos', 'mbenegas', 'Benegas', '$2y$12$ijB2O2ILfTR1mAOMSaUnguhfn/Dsa02MLuqBt1mQZ29CLO247VL1.', 'correo@correo.com', NULL, '2024-12-18 04:20:24', '2024-12-18 04:20:24');
 
 -- --------------------------------------------------------
 
@@ -87,7 +87,16 @@ CREATE TABLE `categorias` (
 
 INSERT INTO `categorias` (`id_categoria`, `categoria`) VALUES
 (1, 'Mate'),
-(2, 'Mates de Acero');
+(2, 'Mates de Acero'),
+(3, 'Yerbera y azucarera'),
+(4, 'Bombillas de Acero'),
+(5, 'Set Materos'),
+(6, 'Botellas Plásticas'),
+(7, 'Bombillas de Alpaca'),
+(8, 'Bombillas de Acero'),
+(9, 'Latas'),
+(10, 'Acesorio'),
+(11, 'Vaso Térmico');
 
 -- --------------------------------------------------------
 
@@ -105,9 +114,10 @@ CREATE TABLE `destacados` (
 --
 
 INSERT INTO `destacados` (`id_destacados`, `id_prodDest`) VALUES
-(1, 1),
-(2, 2),
-(3, 4);
+(3, 4),
+(14, 5),
+(11, 38),
+(12, 39);
 
 -- --------------------------------------------------------
 
@@ -137,17 +147,22 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 CREATE TABLE `nombrelink` (
   `id_link` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
-  `nombreLink` varchar(50) NOT NULL
+  `nombre_Link` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `nombrelink`
 --
 
-INSERT INTO `nombrelink` (`id_link`, `id_producto`, `nombreLink`) VALUES
+INSERT INTO `nombrelink` (`id_link`, `id_producto`, `nombre_Link`) VALUES
 (1, 1, 'mate-acero-termico'),
 (3, 3, 'mate-de-ceramica-artesanal'),
-(4, 4, 'mate-de-madera-tallada-con-bombilla-incluida');
+(4, 4, 'mate-de-madera-tallada-con-bombilla-incluida'),
+(5, 27, 'mate-de-algarrobo'),
+(6, 28, 'mate-de-ceramico'),
+(7, 29, 'mate-urbano'),
+(16, 38, 'set-de-latas'),
+(17, 39, 'set-de-latas-con-mate');
 
 -- --------------------------------------------------------
 
@@ -172,10 +187,10 @@ CREATE TABLE `productos` (
   `cantidad` int(11) NOT NULL,
   `descripcion` varchar(255) NOT NULL,
   `estado` tinyint(4) NOT NULL,
+  `eliminado` tinyint(4) NOT NULL,
   `precio` float NOT NULL,
-  `fecha_creacion` varchar(20) NOT NULL,
-  `fecha_modificacion` varchar(20) NOT NULL,
-  `p_link` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
   `imgUrl` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -183,14 +198,20 @@ CREATE TABLE `productos` (
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_mate`, `cantidad`, `descripcion`, `estado`, `precio`, `fecha_creacion`, `fecha_modificacion`, `p_link`, `imgUrl`) VALUES
-(1, 1, 'Mate Acero Térmico – MATERO MARWAL XL', 1, 55, '22/11/2024', '', '', NULL),
-(2, 1, 'Mate Acero Térmico – MATERO MARWAL XL', 1, 5500, '22/11/2024', '', '', NULL),
-(3, 50, 'Mate de cerámica artesanal', 0, 350, '2024-12-01', '2024-12-01', 'https://ecommerce.com/mate-ceramica', 'https://ecommerce.com/img/mate-ceramica.jpg'),
-(4, 30, 'Mate de madera tallada con bombilla incluida', 0, 450, '2024-12-01', '2024-12-02', 'https://ecommerce.com/mate-madera', 'https://ecommerce.com/img/mate-madera.jpg'),
-(5, 20, 'Mate de vidrio con detalles en acero inoxidable', 0, 500, '2024-12-01', '2024-12-02', 'https://ecommerce.com/mate-vidrio', 'https://ecommerce.com/img/mate-vidrio.jpg'),
-(6, 10, 'Set de mate premium con yerbera y bombilla', 0, 1200, '2024-12-01', '2024-12-03', 'https://ecommerce.com/set-mate-premium', 'https://ecommerce.com/img/set-mate-premium.jpg'),
-(7, 40, 'Mate de acero inoxidable con doble capa térmica', 0, 800, '2024-12-01', '2024-12-03', 'https://ecommerce.com/mate-acero', 'https://ecommerce.com/img/mate-acero.jpg');
+INSERT INTO `productos` (`id_mate`, `cantidad`, `descripcion`, `estado`, `eliminado`, `precio`, `created_at`, `updated_at`, `imgUrl`) VALUES
+(1, 3, 'Mate Acero Térmico', 1, 0, 50000, '0000-00-00 00:00:00', '2024-12-25 21:02:50', 'img/1735160570_28.png'),
+(2, 1, 'Mate Acero Térmico – MATERO MARWAL XL', 1, 0, 5500, '0000-00-00 00:00:00', '2024-12-25 21:03:04', 'img/1735160584_38.jpg'),
+(3, 50, 'Mate de cerámica artesanal', 1, 1, 350, '2024-12-01 00:00:00', '2024-12-23 15:26:03', 'https://ecommerce.com/img/mate-ceramica.jpg'),
+(4, 30, 'Mate de madera tallada con bombilla incluida', 1, 1, 450, '2024-12-01 00:00:00', '2024-12-23 15:27:53', 'https://ecommerce.com/img/mate-madera.jpg'),
+(5, 20, 'Mate de vidrio con detalles en acero inoxidable', 1, 0, 500, '2024-12-01 00:00:00', '2024-12-26 02:07:54', 'https://ecommerce.com/img/mate-vidrio.jpg'),
+(6, 10, 'Set de mate premium con yerbera y bombilla', 0, 1, 1200, '2024-12-01 00:00:00', '2024-12-25 21:38:10', 'https://ecommerce.com/img/set-mate-premium.jpg'),
+(7, 40, 'Mate de acero inoxidable con doble capa térmica', 0, 1, 800, '2024-12-01 00:00:00', '2024-12-23 23:08:36', 'https://ecommerce.com/img/mate-acero.jpg'),
+(8, 3, 'Mate de acero', 1, 1, 20000, '2024-12-22 20:28:01', '2024-12-23 15:25:10', NULL),
+(27, 20, 'Mate de algarrobo', 1, 1, 10000, '2024-12-22 21:16:38', '2024-12-23 15:25:55', NULL),
+(28, 3, 'mate de ceramico', 1, 0, 10000, '2024-12-22 21:52:20', '2024-12-22 21:52:20', NULL),
+(29, 20, 'mate urbano', 1, 0, 10000, '2024-12-22 21:56:16', '2024-12-22 21:56:16', NULL),
+(38, 3, 'set de latas', 1, 0, 10000, '2024-12-24 00:42:36', '2024-12-24 01:14:23', 'img/1735000956_5-5-600x600.webp'),
+(39, 3, 'set de latas con mate', 1, 0, 10000, '2024-12-24 17:06:05', '2024-12-25 22:08:56', 'img/1735164536_4012.webp');
 
 -- --------------------------------------------------------
 
@@ -210,7 +231,17 @@ CREATE TABLE `producto_categoria` (
 
 INSERT INTO `producto_categoria` (`id_categoria`, `id_producto`, `id_rel`) VALUES
 (1, 1, 1),
-(2, 2, 2);
+(1, 28, 4),
+(1, 29, 5),
+(4, 38, 14),
+(1, 39, 32),
+(1, 6, 33),
+(9, 6, 34),
+(2, 39, 35),
+(3, 39, 36),
+(5, 39, 37),
+(9, 39, 38),
+(1, 5, 39);
 
 -- --------------------------------------------------------
 
@@ -232,7 +263,8 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('H584gLUUBNujXgk2U5BKBIhQq1ctIX3pGHGK0Dex', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiU1hBeUFYdlhjVEdqR0tiQ3NzZXVrTWlOcXRJYTFmWXM0OUZwSVJxayI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9hZG1pbmlzdHJhY2lvbi9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1734495185);
+('0zWmdmYyLBeyycxTwRhxerJpQpDbdbQKxhIPHOyV', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoibXR0MmNDM0pqdTFxdXlEaWJ3UzZzT3JpczdxeGFzV25TSFlTWUJVYyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MjtzOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czo0NjoiaHR0cDovL2xvY2FsaG9zdDo4MDAwL2FkbWluaXN0cmFjaW9uL2Rhc2hib2FyZCI7fX0=', 1735188655),
+('oOE7JvWudPECnWQMgs1FLBN1jugJPGHLLQsZDxaP', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiMkFVVWdxMnFLMzhPQmgwU00wZUpON29qQU9jcGZKM2h5d21sWTZxQiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9jb250YWN0byI7fX0=', 1735230500);
 
 -- --------------------------------------------------------
 
@@ -342,19 +374,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `admin_user`
 --
 ALTER TABLE `admin_user`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de la tabla `destacados`
 --
 ALTER TABLE `destacados`
-  MODIFY `id_destacados` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_destacados` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
@@ -366,19 +398,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT de la tabla `nombrelink`
 --
 ALTER TABLE `nombrelink`
-  MODIFY `id_link` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_link` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_mate` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_mate` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `producto_categoria`
 --
 ALTER TABLE `producto_categoria`
-  MODIFY `id_rel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_rel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -400,14 +432,14 @@ ALTER TABLE `destacados`
 -- Filtros para la tabla `nombrelink`
 --
 ALTER TABLE `nombrelink`
-  ADD CONSTRAINT `fk_link` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_mate`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_link` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_mate`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `producto_categoria`
 --
 ALTER TABLE `producto_categoria`
-  ADD CONSTRAINT `fk_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_mate`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_id_producto` FOREIGN KEY (`id_producto`) REFERENCES `productos` (`id_mate`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
