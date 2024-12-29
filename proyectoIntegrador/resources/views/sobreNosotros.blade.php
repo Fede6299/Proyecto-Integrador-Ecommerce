3f68a02706aed429720acb3d6b3c977e356f6db5
Gabriela Choque <nadehsko@hotmail.com>
2024-12-29 17:56:02 -0300

    Modificación en la vista de sobreNosotros.

--- proyectoIntegrador/resources/views/sobreNosotros.blade.php
+++ proyectoIntegrador/resources/views/sobreNosotros.blade.php
@@ -1,42 +1,41 @@
+
 @include('/layout/header')
 <div class="container marketing ">
     <h2 class="tituloNosotros">Sobre nosotros </h2>
-    <p class="lead main-text">En [Nombre de la Empresa], vivimos y respiramos la pasión por el mate. Desde nuestros inicios como una pequeña empresa familiar, nos hemos dedicado a ofrecer productos que honran esta tradición tan arraigada en nuestra cultura.
+    <div class="divisionSobreNosotros"></div>
+    <p class="lead main-text presentacionSobreNosotros">En [Nombre de la Empresa], vivimos y respiramos la pasión por el mate. Desde nuestros inicios como una pequeña empresa familiar, nos hemos dedicado a ofrecer productos que honran esta tradición tan arraigada en nuestra cultura.
             Creemos que el mate no es solo una bebida; es un símbolo de amistad, encuentros y momentos compartidos. Por eso, trabajamos cada día para brindarte una selección de productos diseñados con calidad, estilo y funcionalidad. Desde mates de madera y calabaza hasta bombillas de acero, sets materos y accesorios, cada pieza refleja nuestro compromiso con la excelencia.
             Nuestro equipo está conformado por personas apasionadas que valoran tanto el diseño como la practicidad. Nos enorgullece apoyar a artesanos locales y fomentar el uso de materiales sostenibles para cuidar nuestro planeta mientras disfrutamos de esta tradición.
             Acompáñanos en este viaje mate por mate, y permitamos que esta experiencia continúe uniendo corazones. ¡Gracias por elegirnos como parte de tus momentos más especiales!</p>
-  
-
-
 
     <div class="row ">
         
         <div class="col-lg-3">
                  <img src="./img/fede.png" alt="Descripción de la imagen" class="bd-placeholder-img rounded-circle" width="140" height="140">
-                 <h2 class="fw-normal">Fede</h2>
-                 <p>Descripción breve</p>
-                 <p><a class="btn btn-secondary" href="#">Mas detalles (?) »</a></p>
+                 <h2 class="tituloCadaIntegrante">Fede</h2>
+                 <p class="descripcion">Descripción breve</p>
+                 <p><a class="btn btn-secondary" href="#">Descargar CV »</a></p>
         </div>
   
          <div class="col-lg-3">
                 <img src="./img/gab.png" alt="Descripción de la imagen" class="bd-placeholder-img rounded-circle" width="140" height="140">
-                <h2 class="fw-normal">Gabi</h2>
-                <p>Descripción breve</p>
-                <p><a class="btn btn-secondary" href="#">Mas detalles (?) »</a></p>
+                <h2 class="tituloCadaIntegrante">Gabi</h2>
+                <p class="descripcion">Encargada del diseño y desarrollo frontend, se encargó de dar estilo a cada sección de la página. Aseguró que la interfaz sea clara y funcional para brindar una experiencia óptima al usuario.</p>
+                <p><a class="btn btn-secondary" href="#">Descargar CV »</a></p>
          </div>
   
         <div class="col-lg-3">
                 <img src="./img/marcos.png" alt="Descripción de la imagen" class="bd-placeholder-img rounded-circle" width="140" height="140">
-                <h2 class="fw-normal">Marcos</h2>
-                <p>Descripción breve</p>
-                <p><a class="btn btn-secondary" href="#">Mas detalles (?) »</a></p>
+                <h2 class="tituloCadaIntegrante">Marcos</h2>
+                <p class="descripcion">Encargado de la administración de la base de datos, integró la comunicación entre el backend y frontend. Además, lideró el desarrollo implementando funcionalidades clave para el proyecto.</p>
+                <p><a class="btn btn-secondary botonSobreNosotros" href="#">Descargar CV »</a></p>
         </div>
 
         <div class="col-lg-3">
                 <img src="./img/nico.jpg" alt="Descripción de la imagen" class="bd-placeholder-img rounded-circle" width="140" height="140">
-                <h2 class="fw-normal">Nico</h2>
-                <p>Descripción breve</p>
-                <p><a class="btn btn-secondary" href="#">Mas detalles (?) »</a></p>
+                <h2 class="tituloCadaIntegrante">Nico</h2>
+                <p class="descripcion">Descripción breve</p>
+                <p><a class="btn btn-secondary botonSobreNosotros" href="#">Descargar CV »</a></p>
         </div>
     </div><!-- /.row -->
     
@@ -44,3 +43,4 @@
 </div><!-- /.container -->
 
 @include('/layout/footer')
+
